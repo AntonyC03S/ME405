704830507b62f543a24f369ff9ba6419d8348937
Antony Chen <122947939+AntonyC03S@users.noreply.github.com>
2025-10-01 21:35:13 -0700

    Updated Code

--- Lab_0x00_graph.py
+++ Lab_0x00_graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pylab as plt
 import math
+import numpy as np
 
 def Extract_Data_CSV(file = "data.csv"):
     x_values = []
@@ -12,27 +13,6 @@ def Extract_Data_CSV(file = "data.csv"):
     trial7 = []
 
     with open(file, 'r') as file:
-        title = file.readline()
-        for line in file:
-            try:
-                float(line[0])
-                lineparts = line.split(",")
-                x_values.append(float(lineparts[0]))
-                trial1.append(float(lineparts[1]))
-                trial2.append(float(lineparts[2]))
-                trial3.append(float(lineparts[3]))
-                trial4.append(float(lineparts[4]))
-                trial5.append(float(lineparts[5]))
-                trial6.append(float(lineparts[6]))
-                trial7.append(float(lineparts[7]))
-            except:
-                continue
-    return x_values, trial1, trial2, trial3, trial4, trial5, trial6, trial7, title
-
-def test():
-    times = []            
-    height = []
-    with open('data.csv', 'r') as file:
         # First line is always the column labels
         title = file.readline()
 
@@ -53,10 +33,14 @@ def test():
                 
                 # Line gets split to each column data 
                 lineparts = line.split(",")
-                times.append(float(lineparts[0]))
-                heightdata = lineparts[1].split("#")
-                height.append(float(heightdata[0]))
-
+                x_values.append(float(lineparts[0]))
+                trial1.append(float(lineparts[1]))
+                trial2.append(float(lineparts[2]))
+                trial3.append(float(lineparts[3]))
+                trial4.append(float(lineparts[4]))
+                trial5.append(float(lineparts[5]))
+                trial6.append(float(lineparts[6]))
+                trial7.append(float(lineparts[7]))
             # Word Data Error
             except ValueError:
                 print(f"Line {row+2}: Data is not numbers.")
@@ -64,6 +48,7 @@ def test():
             # All the other Errors
             except:
                 continue
+    return x_values, trial1, trial2, trial3, trial4, trial5, trial6, trial7, title
 
 
 def main():  
@@ -84,18 +69,25 @@ def main():
 
 
     plt.figure()
-    V_in = 3770
-    R = 30000
-    C = 0.000001
+    V_in = 3770    # mV
+    R = 34190      # Ω
+    C = 0.00000428 # F
+    limit = 100
 
+    all_time = time[0:limit] * 7
+    time_theoretical = time[0:limit]
+    all_trails = trial1[100:100+limit] + trial2[100:100+limit] + trial3[100:100+limit] + trial4[100:100+limit] + trial5[100:100+limit] + trial6[100:100+limit] + trial7[100:100+limit]
+    y = [math.log(1-(Vout)/(V_in)) for Vout in all_trails]
+    y_theoretical = [ (-t/(R*C)) for t in time_theoretical]
 
-    y_1 = [math.log(1-(Vout)/(V_in)) for Vout in trial1]
-    y_2 = [math.log(1-(Vout)/(V_in)) for Vout in trial2]
-    y_3 = [math.log(1-(Vout)/(V_in)) for Vout in trial3]
-    x = [-t/(R*C) for t in time]
-    plt.scatter(x, y_1, marker='o', color='red', s=5)
-    plt.scatter(x, y_2, marker='o', color='blue', s=5)
-    plt.scatter(x, y_3, marker='o', color='green', s=5)    
+    plt.scatter(all_time, y, marker='o', color='black', s=5)
+    plt.scatter(time_theoretical, y_theoretical, marker='o', color='red', s=2)
+    plt.legend(['Experimental', 'Theoretical'])
+
+    # Setting intercept to 0 and getting the slope 
+    A = np.array(all_time).reshape(-1, 1)
+    m = np.linalg.lstsq(A, y, rcond=None)[0][0]
+    print(-1/m)
     plt.show()
 
 
