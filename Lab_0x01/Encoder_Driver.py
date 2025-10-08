80db4ae5fb33531fd74c82fbf2d98138425ea7ef
Antony Chen <122947939+AntonyC03S@users.noreply.github.com>
2025-10-08 15:00:49 -0700

    Update Lab0x01

--- Lab_0x01/Encoder_Driver.py
+++ Lab_0x01/Encoder_Driver.py
@@ -6,28 +6,47 @@ class Encoder:
 
     def __init__(self, tim: Timer, chA_pin: Pin, chB_pin: Pin):
         '''Initializes an Encoder object'''
-    
-        self.position   = 0     # Total accumulated position of the encoder
-        self.prev_count = 0     # Counter value from the most recent update
-        self.delta      = 0     # Change in count between last two updates
-        self.dt         = 0     # Amount of time between last two updates
+
+        self._ChA_chan = tim.channel(0, pin = chA_pin, mode=Timer.PWM, pulse_width_percent=0)
+        self._CHB_chan = tim.channel(1, pin = chB_pin, mode=Timer.PWM, pulse_width_percent=0)
+        self._position   = None   # Total accumulated position of the encoder
+        self._prev_count = 0      # Counter value from the most recent update
+        self._delta      = 0      # Change in count between last two updates
+        self._dt         = 0      # Amount of time between last two updates
+        self._AR         = 65,535 # 16-bit timers maximal count value
     
     def update(self):
         '''Runs one update step on the encoder's timer counter to keep
            track of the change in count and check for counter reload'''
-        self.delta = 0 
+        
+        if self._position is None:
+            self._position == ticks_us()
+            return
+        
+        now = ticks_us()
+        self._delta = ticks_diff(self._position , now)
+        # Overflow: delta < -(AR+1)/2; to correct, delta += AR + 1 
+        if self._delta < -(self._AR+1)/2:
+            self._delta += self._AR +1
+
+        # Underflow: delta > (AR+1)/2; to correct, delta -= AR + 1 
+        elif self._delta > (self._AR+1)/2:
+            self._delta -= self._AR +1
             
-    def get_position(self):
+
+    @property
+    def position(self):
         '''Returns the most recently updated value of position as determined
            within the update() method'''
-        return self.position
-            
-    def get_velocity(self):
+        return f"{self._position}"
+    
+    @property   
+    def velocity(self):
         '''Returns a measure of velocity using the the most recently updated
            value of delta as determined within the update() method'''
-        return self.delta/self.dt
+        return f"{self._delta/self._dt}"
     
     def zero(self):
         '''Sets the present encoder position to zero and causes future updates
            to measure with respect to the new zero position'''
-        self.position = 0
+        self._position = 0
