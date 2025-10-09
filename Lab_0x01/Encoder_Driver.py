b81c47336d8685b5b4a2ea97e1d2ee6438706357
Antony Chen <122947939+AntonyC03S@users.noreply.github.com>
2025-10-08 17:03:15 -0700

    Update Encoder_Driver.py

--- Lab_0x01/Encoder_Driver.py
+++ Lab_0x01/Encoder_Driver.py
@@ -4,13 +4,15 @@ from time import ticks_us, ticks_diff   # Use to get dt value in update()
 class Encoder:
     '''A quadrature encoder decoding interface encapsulated in a Python class'''
 
-    def __init__(self, tim: Timer, chA_pin: Pin, chB_pin: Pin, chA_chan: int, chB_chan: int):
+    def __init__(self, tim: Timer, chA_pin: Pin, chB_pin: Pin, chA_chan: int = 1, chB_chan: int = 2):
         '''Initializes an Encoder object'''
 
-        self._ChA_chan = tim.channel(chA_chan, pin = chA_pin, mode=Timer.PWM, pulse_width_percent=0)
-        self._CHB_chan = tim.channel(chB_chan, pin = chB_pin, mode=Timer.PWM, pulse_width_percent=0)
-        self._position   = 0   # Total accumulated position of the encoder
+        self._tim = tim
+        self._ChA_chan = tim.channel(chA_chan, pin = chA_pin, mode=Timer.ENC_AB)
+        self._CHB_chan = tim.channel(chB_chan, pin = chB_pin, mode=Timer.ENC_AB)
+        self._position   = 0      # Total accumulated position of the encoder
         self._prev_count = 0      # Counter value from the most recent update
+        self._prev_time  = 0
         self._delta      = 0      # Change in count between last two updates
         self._dt         = 0      # Amount of time between last two updates
         self._AR         = 65,535 # 16-bit timers maximal count value
@@ -19,12 +21,7 @@ class Encoder:
         '''Runs one update step on the encoder's timer counter to keep
            track of the change in count and check for counter reload'''
         
-        if self._position is None:
-            self._position == ticks_us()
-            return
-        
-        now = ticks_us()
-        self._delta = ticks_diff(self._position , now)
+        self._delta = ticks_diff(self._prev_count , self._tim.counter())
         # Overflow: delta < -(AR+1)/2; to correct, delta += AR + 1 
         if self._delta < -(self._AR+1)/2:
             self._delta += self._AR +1
@@ -32,6 +29,13 @@ class Encoder:
         # Underflow: delta > (AR+1)/2; to correct, delta -= AR + 1 
         elif self._delta > (self._AR+1)/2:
             self._delta -= self._AR +1
+
+        self._position += self._delta
+
+
+        now = ticks_us()
+        self._dt = ticks_diff(now, self._prev_time)
+        self._prev_time == now
             
 
     @property
