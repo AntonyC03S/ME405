7ec691b7df7ff72ca313ea7952a1a40ce0a0e552
Antony Chen <achen321@calpoly.edu>
2025-10-30 03:29:07 -0700

    Updated Graph

--- Lab_0x03/Lab_0x03_report.py
+++ Lab_0x03/Lab_0x03_report.py
@@ -5,9 +5,10 @@ import math
 import numpy as np
 
 def main():
-    folder_path = os.path.join("Lab_0x03", "data")
+    folder_path = os.path.join("Lab_0x03", "data_5")
+    # fig, axes = plt.subplots(2, 1, figsize=(7, 5), sharex=True)
+    # plt.subplots_adjust(right=0.78)  
     plt.figure()
-    # Loop through each CSV file in the folder
     for filename in sorted(os.listdir(folder_path)):
         left_speed = []
         right_speed = []
@@ -28,11 +29,29 @@ def main():
                     left_poistion.append(float(row[1]))
                     right_poistion.append(float(row[2]))
         
-        #plt.plot(time, right_speed, label = f"{file_path}")
-        plt.plot(time, left_speed, label = f"{file_path}")
-    plt.legend()
-    plt.show()
+        File_name = filename.split(".csv")
+        plt.plot(time, right_speed, label = f"{File_name[0]} Right")
+        plt.plot(time, left_speed, label = f"{File_name[0]} Left")
 
+    plt.xlim(0,5)
+    plt.title("PID Tuning Both side", fontsize = 30)
+    plt.xlabel("Time, t[s]")
+    plt.ylabel("Velocity, Ω[rad/s]")
+    plt.legend(["P2_I19_D0.1 Right","P2.5_I20_D0.1 Left","P2_I19_D0.1 Right","P2.7_I20_D0.1 Left"])
+    plt.show
+
+    
+    #     axes[0].plot(time, right_speed, label = f"{File_name[0]}")
+    #     axes[1].plot(time, left_speed)
+    # fig.legend(loc='center left', bbox_to_anchor=(0.82, 0.5), title="Legend",edgecolor='black')
+    # fig.suptitle("PID Testing", fontsize = 30)
+    # axes[0].set_xlim(0,10)
+    # axes[0].set_title("Right Motor")
+    # axes[1].set_title("Left Motor")
+    # axes[0].set_ylabel("Velocity, Ω[rad/s]")
+    # axes[1].set_xlabel("Time, t[s]") 
+    # axes[1].set_ylabel("Velocity, Ω[rad/s]")
+    plt.show()
 
 if __name__ == "__main__":
     main()
