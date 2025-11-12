b0a2f15f58842db2d2207eabd097d83bda97e344
israelvillegas123 <israelvil08@gmail.com>
2025-11-12 14:44:14 -0800

    hi

--- Lab_0x05/IMU_test.py
+++ Lab_0x05/IMU_test.py
@@ -1,5 +1,6 @@
-from pyb import Pin, I2C
-from imu_driver import IMU
+from pyb import I2C
+from imu_Class import IMU
+import time
 
 # Create I2C bus 2 (Y9=SCL, Y10=SDA)
 i2c = I2C(2, I2C.CONTROLLER, baudrate=400000)
@@ -7,6 +8,9 @@ i2c = I2C(2, I2C.CONTROLLER, baudrate=400000)
 # Create IMU instance
 imu = IMU(i2c)
 
+
+# Read continuously
 while True:
-    hi = imu.scan()
-    print(hi)
+    acc = imu.read('Acc')
+    print("Accel (m/s²):", acc)
+    time.sleep(0.5)
